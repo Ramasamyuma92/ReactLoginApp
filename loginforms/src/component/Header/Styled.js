476d0ad4b1250ceff9acde0c1640e1e13d7506ef
Arun S <sarun61@outlook.com>
2021-10-10 22:10:56 +0530

    feat: added header content

---
+++ loginforms/src/component/Header/Styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+    display: flex;
+    width: 100%;
+    height: 6.5rem;
+    flex-direction: row;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    background: rgb(254, 254, 254);
+    padding: 0px 1.5rem;
+    border-bottom: 1px solid rgb(237, 237, 237);
+`;
